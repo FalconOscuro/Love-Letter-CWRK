fa784ae4b0dc523726de52bcf14eded2b49a4252
Peter Garrod <majeika@gmail.com>
2021-02-03 09:41:21 +0000

    Add project files.

---
+++ Love Letter CWRK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Love_Letter_CWRK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
