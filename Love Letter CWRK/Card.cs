90f449bf6a3e958b505769665186e0ab921df898
Peter Garrod <majeika@gmail.com>
2021-02-03 10:06:08 +0000

    Created virtual card class.

---
+++ Love Letter CWRK/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Love_Letter_CWRK
+{
+    /*
+     * This is a purely virtual class describing & inherited by all cards in the game
+    */
+    class Card
+    {
+        public Card()
+        {}
+
+        /*
+         * Run when a player plays this card
+         * TODO: Needs to take in arguments of the target player and the owner of this card
+        */
+        public virtual void Play()
+        {}
+
+        /*
+         * The number value associated with this type of card
+        */
+        public const uint m_Value = 0;
+
+        /*
+         * This cards name
+        */
+        public const string m_Name = "";
+
+        /*
+         * Plaintext describing the effect of the card when the play function is run
+        */ 
+        public const string m_EffectText = "";
+    }
+}
