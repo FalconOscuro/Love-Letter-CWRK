f54038fda2e7c0cd50098713af3b5dbfcfaa3791
Peter Garrod <majeika@gmail.com>
2021-05-06 02:00:27 +0100

    Created game manager class with default starting deck

--- Love Letter CWRK/GameManager.cs
+++ Love Letter CWRK/GameManager.cs
@@ -6,5 +6,16 @@ namespace Love_Letter_CWRK
 {
     class GameManager
     {
+
+        static Card[] StartingDeck = {
+            new Cards.Guard(), new Cards.Guard(), new Cards.Guard(), new Cards.Guard(), new Cards.Guard(),
+            new Cards.Priest(), new Cards.Priest(),
+            new Cards.Baron(), new Cards.Baron(),
+            new Cards.Handmaid(), new Cards.Handmaid(),
+            new Cards.Prince(), new Cards.Prince(),
+            new Cards.King(),
+            new Cards.Countess(),
+            new Cards.Princess()
+        };
     }
 }
