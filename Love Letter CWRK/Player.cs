2e267069c64dd12060696cc52fc6abe76c4cd684
Peter Garrod <majeika@gmail.com>
2021-05-06 02:42:06 +0100

    Added various getter and setter functions

--- Love Letter CWRK/Player.cs
+++ Love Letter CWRK/Player.cs
@@ -1,31 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Love_Letter_CWRK
+﻿namespace Love_Letter_CWRK
 {
     /*
      * Class describing an individual player within the game
-    */ 
-    class Player
+    */
+    internal class Player
     {
         /*
-         * Default constructor
-        */ 
+         * Constructor to set variables to their default values
+        */
         public Player()
-        { }
+        {
+            m_Hand = new Card[2];
+            m_Tokens = 0;
+            m_Out = false;
+            m_Targetable = true;
+        }
 
         /*
          * Empties the players hand and discard pile ready for a new round
-        */ 
+        */
         public void Cleanup()
         { }
+
+        /*
+         * Adds a card to the players hand
+        */
+        public bool AddCard(Card card)
+        {
+            if (m_Hand[1] == new Card())
+                return false;
+
+            m_Hand[1] = card;
+
+            return true;
+        }
+
+        /*
+         * Getter function for a player's hand
+        */
+        public ref Card[] GetHand()
+        {
+            return ref m_Hand;
+        }
+
+        public bool IsOut()
+        {
+            return m_Out;
+        }
+
+        public void SetOut(bool state)
+        {
+            m_Out = state;
+        }
+
+        public bool IsTargetable()
+        {
+            return !m_Out && m_Targetable;
+        }
+
+        public void SetTargetable(bool state)
+        {
+            m_Targetable = state;
+        }
 
         /// Private attributes
         /*
          * An array of the cards currently in this players hand
          * Always initialized to a length of 2 as that is the maximum hand size
-        */ 
+        */
         private Card[] m_Hand;
 
         /*
@@ -37,5 +79,9 @@ namespace Love_Letter_CWRK
          * The current number of tokens that this player has
         */
         private int m_Tokens;
+
+        private bool m_Out;
+
+        private bool m_Targetable;
     }
 }
